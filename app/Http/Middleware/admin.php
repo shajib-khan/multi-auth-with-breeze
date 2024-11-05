2f671577af801cd859ef4f44008b91cf931acfd8
shajib-khan <shajibkhan946@gmail.com>
2024-11-05 19:33:32 +0600

    multi auth done now

---
+++ app/Http/Middleware/admin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+class admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //if user not logged in
+        if(!Auth::check()){
+            return redirect()->route('login');
+        }
+        $userRole = Auth::user()->role;
+        //super admin
+        if ($userRole == 1){
+            return redirect()->route('super.admin.dashboard');
+        }
+          //Admin
+        elseif ($userRole ==2){
+            return $next($request);
+    }
+     //normal user
+         elseif($userRole == 3){
+        return redirect()->route('user.dashboard');
+    }
+    }
+
+}
